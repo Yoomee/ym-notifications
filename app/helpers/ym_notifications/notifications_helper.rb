938ee4b909e81bf5978535c096d5845ddcb88721
Edward Andrews-Hodgson <edward@yoomee.com>
2014-01-15 15:12:58 +0000

    Allow notification helper to be used in mailer urls not paths and setting current_user

--- app/helpers/ym_notifications/notifications_helper.rb
+++ app/helpers/ym_notifications/notifications_helper.rb
@@ -12,20 +12,20 @@ module YmNotifications::NotificationsHelper
     resource ? link_to(image_for(resource, '60x60#'), resource, :class => 'notification-img') : ''
   end
 
-  def link_to_notification_resource(notification)
-    link_to("view #{notification.resource_type.humanize.downcase}", notification.resource_path, :icon => 'arrow-right')
+  def link_to_notification_resource(notification, user = current_user)
+    link_to("view #{notification.resource_type.humanize.downcase}", polymorphic_url(notification.resource), :icon => 'arrow-right')
   end
 
-  def notification_description(notification)
+  def notification_description(notification, user = current_user)
     case notification.resource_type
     when 'Comment'
-      link_to_self(notification.resource.user) +
+      link_to(notification.resource.user, user_url(notification.resource.user)) +
       ' commented on ' +
-      link_to("#{nickname_or_your(notification.resource.post.user, :their_user => notification.resource.user)} post", notification.resource)
+      link_to("#{nickname_or_your(notification.resource.post.user, :their_user => notification.resource.user)} post", polymorphic_url(notification.resource))
     when 'Post'
       link_to_self(notification.resource.user) +
       ' posted on ' +
-      link_to('your profile', current_user)
+      link_to('your profile', user_url(user))
     end
   end
 
